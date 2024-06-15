9146c17dffd20fc54fc930dbf1961703d23b9690
Nic11371 <nic11371@gmail.com>
2024-06-15 11:09:48 +0300

    step_3_finished

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -5,9 +5,13 @@ from brain_games.cli import welcome_user
 
 def welcome():
     print("Welcome to the Brain Games!")
+
+
 def main():
     welcome()
+
     welcome_user()
-    
+
+
 if __name__ == '__main__':
-   main()
+    main()
