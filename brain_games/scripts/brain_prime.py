7a1be4e2505b4b0c01e9f191bc21e60e65276bbe
Nic11371 <nic11371@gmail.com>
2024-06-19 22:36:44 +0300

    Created brain-prime. Upload Readme.md

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import brain_games.engine as engine
+import brain_games.games.prime as prime
+
+
+def main():
+    engine.engine(prime)
+
+
+if __name__ == '__main__':
+    main()
