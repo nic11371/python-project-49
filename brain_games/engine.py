9cbb2dd6bad07e93e4bae1cf7fff687ac563b2fc
Nikolay Melnikov <nic11371@gmail.com>
2024-06-20 17:50:09 +0300

    Fix engine.messages

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,11 +3,11 @@ import brain_games.cli as cli
 
 
 messages = {
-    "even": "Answer 'yes' if the number is even, otherwise answer 'no'.",
+    "even": "Answer \"yes\" if the number is even, otherwise answer \"no\".",
     "calc": "What is the result of the expression?",
     "gcd": "Find the greatest common divisor of given numbers.",
     "progression": "What number is missing in the progression?",
-    "prime": "Answer 'yes' if given number is prime. Otherwise answer 'no'."
+    "prime": "Answer \"yes\" if given number is prime. Otherwise answer \"no\"."
 }
 
 
