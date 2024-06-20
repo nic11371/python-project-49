6ba28787c3f9e23eca7612e07f773b7a6d257a6e
Nic11371 <nic11371@gmail.com>
2024-06-20 20:06:19 +0300

    Fix progression 2

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,8 +3,8 @@ from random import randint, choice
 
 def logic_function():
     sequence = []
-    start = randint(1, 10)
-    stop = randint(6, 10)
+    start = randint(0, 10)
+    stop = randint(5, 10)
     step = randint(1, 10)
     for i in range(stop):
         value = start + step
