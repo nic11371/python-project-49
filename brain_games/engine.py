6f44f8f37e52c801f80dc64969160066a598cb96
Nic11371 <nic11371@gmail.com>
2024-06-24 20:03:26 +0300

    Fix variable, add const

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -2,18 +2,21 @@ from prompt import string
 from brain_games.cli import welcome
 
 
+MAX_ROUND = 3
+
+
 def engine(game_module):
     name_user = welcome()
     print(game_module.RULE)
-    count = 3
-    while count > 0:
-        current_value, corr = game_module.make_logic()
+    current_round = 0
+    while current_round < MAX_ROUND:
+        current_value, correct_value = game_module.make_logic()
         print(f"Question: {current_value}")
-        answer = string("Your answer: ")
-        if answer != str(corr):
-            print(f"'{answer}' is wrong answer ;(.Correct answer was '{corr}'.")
+        answer_user = string("Your answer: ")
+        if answer_user != str(correct_value):
+            print(f"'{answer_user}' is wrong answer ;(.Correct answer was '{correct_value}'.")
             print(f"Let's try again, {name_user}!")
             return
         print("Correct")
-        count -= 1
+        current_round += 1
     print(f"Congratulations, {name_user}!")
