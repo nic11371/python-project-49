b5cb467dc972975eeba9eb6ed10235a0415c95a0
Nic11371 <nic11371@gmail.com>
2024-06-24 20:12:18 +0300

    Fix prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,6 +6,8 @@ RULE = "Answer \"yes\" if given number is prime. Otherwise answer \"no\"."
 
 def make_logic():
     number = randint(1, 100)
+    if number <= 1:
+        current_answer = "no"
     k = 0
     for i in range(2, int(number // 2) + 1):
         if number % i == 0:
